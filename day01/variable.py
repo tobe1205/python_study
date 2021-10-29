b127a249ab798f86058bfcb0dd7294fa5acc1b85
tobe1205 <tobe1205@hanmail.net>
2021-10-29 11:54:15 +0900

    [21.10.29] # 파이썬 기본구조

---
+++ day01/variable.py
@@ -0,0 +1,42 @@
+
+
+n1 = 10
+result = 10 + 20 # = 은 같다라는 의미가 아닌, 대입연사자로, 우측의 항을 좌측에 대입 or 저장한다 라는 의미로 해석
+print(result * 3)
+
+result = n1 + 20
+print(result)
+
+# 선언(make)되지 않은 변수는 사용 불가!
+# result = number + 10
+# print(number)
+print("number") # 따옴표안에 있으면 전부 텍스트로 취급(변수 X)
+
+fruit = '사과'
+
+print(fruit + ' 맛있어요~')
+
+print('=============================')
+
+# 변수 이름 규칙
+# 1. 숫자로 시작하면 안된다. (모든 프로그래밍 공통)
+# 7number = 700
+number7 = 700
+num7ber = 777
+# 2. 공백을 포함할 수 없다.
+# user birth day = 19921205
+userbirthday = 19921205
+user_birth_day = 19921205 # snake case (파이썬 선호)
+UserBirthDay  = 19921205  # camel case (자바, 자바스크립트 선호)
+# 3. 대/소문자를 구분한다.
+banana = '바나나'
+BANANA = '뻐네이너'
+
+print(BANANA)
+# 4. if , while과 같은 이미 기능이 포함된 단어는 변수이름 사용할 수 없다.
+# if = 123
+# for = '메롱'
+
+# 한글, 한자변수 가능? - 가능하지만 안쓰는걸 강력권고!!
+야옹이 = '고양이'
+print(야옹이)
