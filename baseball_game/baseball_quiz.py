3ddcffe1362e6bf9c91a247fcfe66658c0c4da01
tobe1205@hanmail.net <tobe1205@hanmail.net>
2021-11-08 17:21:16 +0900

    숫자 게임 예제

---
+++ baseball_game/baseball_quiz.py
@@ -0,0 +1,56 @@
+import random
+# 1. 난수 설정
+
+a = random.randrange(1,10)
+while True:
+    b = random.randrange(1,10)
+    if(a != b):
+        break
+while True:     
+    c = random.randrange(1,10)
+    if(a != c and b != c):
+        break
+
+count = 0
+
+while True:
+    
+    num1 = int(input('첫번째 숫자: '))
+
+    num2 = int(input('두번째 숫자: '))
+
+    while num1 == num2:
+
+        print('중복된 숫자. 다시입력')
+        
+        num2 = int(input('두번째 숫자: '))
+
+    num3 = int(input('세번째 숫자: '))
+
+    while num3 == num1 or num3 == num2:
+
+        print('중복된 숫자. 다시입력')
+
+        num3 = int(input('세번째 숫자: '))
+    
+    strike = 0
+    ball = 0
+    count += 1
+
+    if a == num1 :
+        strike += 1
+    if b == num2 :
+        strike += 1
+    if c == num3 :
+        strike += 1
+    if a == num2 or a == num3:
+        ball += 1
+    if b == num1 or b == num3:
+        ball += 1
+    if c == num2 or c == num1:
+        ball += 1
+    print(count,'번째 도전')
+    print('strike: ',strike,'ball: ', ball)
+    if strike == 3:
+        break
+print(count,'회 만에 성공 하셨습니다!')
