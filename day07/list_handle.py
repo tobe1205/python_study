7b3d8e796cf163da32c46a78f365a8d49ae923b2
tobe1205 <tobe1205@hanmail.net>
2021-11-09 11:52:38 +0900

    [21.11] # 파이썬 기초 6

--- day07/list_handle.py
+++ day07/list_handle.py
@@ -40,3 +40,43 @@ s ='hello'
 
 s = s.replace('l', 'x', 1)
 print(s)
+
+
+# unpackaging: 리스트 내부 데이터를 다시 변수에 따로 저장
+# 좌항의 변수 갯수와 우항의 리스트 데이터 갯수가 일치해야 함
+
+# tvxq = ['영웅재중', '최강창민', '유노윤호', '믹키유천', '시아준수']
+
+
+hero, max, yuno, micky, xia = tvxq
+print(max)
+
+'''
+hero = tvxq[0]
+max = tvxq[1]
+yuno = tvxq[2]
+micky = tvxq[3]
+xia = tvxq[4]
+'''
+
+
+# 리스트의 +, * 연산
+print('='*40)
+list1 = [1,2,3,4,5]
+list2 = [10,11,12]
+
+print(list1 + list2)
+print(list2 + list1)
+
+print(list2 * 3) # 리스트 * 정수 
+
+print(list1) # 원본은 변하지 않는다.
+
+# 빈 리스트 만들기
+empty = []
+empty = list()
+
+
+
+
+
