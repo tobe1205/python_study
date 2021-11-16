e14336abf70dfefb2eb041d8784028c78f019c0d
tobe1205 <dydrkfl7949@gmail.com>
2021-11-16 16:36:12 +0900

    [21.11.16] # 퀴즈

---
+++ quizzes/quiz1_10.py
@@ -0,0 +1,69 @@
+
+print('Hello World')
+
+print("Mary's cosmetics")
+
+print('신씨가 소리질렀다."도둑이야".')
+
+print("C\Windows")
+
+print("안녕하세요.\n만나서\t\t반갑습니다.")
+# 정답 : 안녕하세요
+# 만나서      반갑습니다.
+
+print('오늘은','일요일')
+# 정답 오늘은 일요일
+
+print('naver','kakao','sk','samsung', sep=';')
+print('naver','kakao','sk','samsung', sep='/')
+print('first', end = " "); print('second')
+
+print(5 / 3)
+
+samsung = 50000
+# 총 평가금액 = 삼성전자 * 10 
+print(f'총 평가금액은 : {samsung * 10}입니다.')
+
+시가총액 = 2980000000000
+현재가 = 5000
+PER = 15.79
+print(시가총액, type(시가총액))
+print(현재가, type(현재가))
+print(PER, type(PER))
+
+s = 'hello'
+t = 'python'
+
+print(s +'!', t)
+
+a = '132'
+print(type(a))
+
+num_str = '720'
+
+print(int(num_str))
+
+num = 100
+
+num = '100'
+result = str(num)
+print(result, type(result))
+
+
+data = "15.79"
+data = float(data)
+print(data, type(data))
+
+letters = 'python'
+
+print(letters[0], letters[2])
+
+license_plate = '24가 2210'
+print(license_plate[-4:])
+
+string = '홀짝홀짝홀짝'
+print(string[::2])
+
+url = "http://sharebook.kr"
+url_split = url.split('.')
+print(url_split[-1])
