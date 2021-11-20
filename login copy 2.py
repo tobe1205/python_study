6d0b49c4d5ae2ead21001e9e44181391377eca3d
tobe1205 <dydrkfl7949@gmail.com>
2021-11-21 01:18:35 +0900

    [팀프로젝트]

--- login copy 2.py
+++ login copy 2.py
@@ -2,7 +2,7 @@ import sys
 # 회원가입 리스트 타입
 user = []
 
-
+print(user)
 # 함수 정의부
 # 메뉴를 출력하는 함수
 def show_menu():
@@ -21,11 +21,10 @@ def user_info():
     print('\n ☆ 회원 가입을 시작합니다. ☆')      
     info['이름'] = input('- 이름: ')
     info['아이디'] = input('- 아이디: ')
-   
-    if info in user:
-        print('이미 등록된 아이디 입니다.')
+    if info['아이디'] in user:
+        print(' 사용가능 아이디 입니다')
     else:
-        print('등록되있지 않은 아이디 입니다')
+        print('이미 등록된 아이디 입니다.')
     info['비밀번호'] = input('- 비밀번호: ')
     if info['비밀번호'] == input('- 비밀번호확인: '):
         print('비밀번호가 일치합니다')
@@ -35,20 +34,28 @@ def user_info():
     user.append(info)
     print('회원가입 되셨습니다!')
     print('메뉴화면으로 돌아가시려면 Enter를 누르세요')
-    print(user)
+    input()
+
 
 def login():
     print('----------로그인----------')
-    while True:
-        info = input('- 아이디: ') 
-        info = input('- 비밀번호: ')
-        if info == user:
+    print(user)
+    for info in user:
+        id = input('- 아이디: ')
+        pw = input('- 비밀번호: ')
+        if (info['아이디'] == id) and (info['비밀번호'] == pw):
             print('☆ 로그인 되셨습니다 ☆')
-            
-        else:         
-            print('로그인 실패! 다시 로그인 해주세요.')
-            break
+            print('{}님 환영합니다.'.format(info['이름']))
+            return
+        elif (info['아이디'] != id) and (info['비밀번호'] == pw):
+            print('아이디가 틀렸습니다.')
+        elif (info['아이디'] == id) and (info['비밀번호'] != pw):
+            print('비밀번호가 틀렸습니다.')
+        
+        input()
+        
 
+    
 # in 키워드를 통해 key 값의 존재 여부를 체크할 수 있음
 
 
@@ -56,9 +63,9 @@ def login():
 if __name__ == '__main__':
     while True: 
         show_menu()
+        print(user)
         menu = int(input('메뉴 입력 = > '))
         if menu == 1:
             user_info()
         if menu == 2:
             login()
-            break
--- login copy.py
+++ login copy.py
@@ -2,7 +2,7 @@ import sys
 # 회원가입 리스트 타입
 user = []
 
-
+print(user)
 # 함수 정의부
 # 메뉴를 출력하는 함수
 def show_menu():
@@ -17,55 +17,78 @@ def show_menu():
 
 # 회원가입
 def user_info():
-    info = {}
-
-    print('\n ☆ 회원 가입을 시작합니다. ☆')      
-    info['이름'] = input('- 이름: ')
-    info['아이디'] = input('- 아이디: ')
-    info['비밀번호'] = input('- 비밀번호: ')
-    print('회원가입 되셨습니다!')
-    print('메뉴화면으로 돌아가시려면 Enter를 누르세요')
-    input()
     
+        info = {}
+        print('\n ☆ 회원 가입을 시작합니다. ☆')      
+        info['이름'] = input('- 이름: ')
+        info['아이디'] = check_duplicate_code()
+        if info['아이디'] not in user:
+            print(' 사용가능 아이디 입니다')
+        else:
+            print('이미 등록된 아이디 입니다.')
+        while True:    
+            info['비밀번호'] = input('- 비밀번호: ')
+            if info['비밀번호'] == input('- 비밀번호확인: '):
+                print('비밀번호가 일치합니다')
+                user.append(info)
+                print('회원가입 되셨습니다!')
+                print('메뉴화면으로 돌아가시려면 Enter를 누르세요')
+                input()
+                return
 
-    if info in user:
-        print('이미 등록된 아이디 입니다.')
-    else:
-        print('등록되있지 않은 아이디 입니다')
-    info['비밀번호'] = input('- 비밀번호: ')
-    if info['비밀번호'] == input('- 비밀번호확인: '):
-        print('비밀번호가 일치합니다')
-    else:
-        print('비밀번호가 일치하지 않습니다.')
-        return
-    user.append(info)
-    print('회원가입 되셨습니다!')
-    print('메뉴화면으로 돌아가시려면 Enter를 누르세요')
- 
+            else:
+                print('비밀번호가 일치하지 않습니다.')
+                continue
+            
+        
+
 
 def login():
     print('----------로그인----------')
-    while True:
-        info = input('- 아이디: ') 
-        info = input('- 비밀번호: ')
-        if info == user:
+    print(user)
+    for info in user:
+        id = input('- 아이디: ')
+        pw = input('- 비밀번호: ')
+        if (info['아이디'] == id) and (info['비밀번호'] == pw):
             print('☆ 로그인 되셨습니다 ☆')
-            
-        else:         
-            print('로그인 실패! 다시 로그인 해주세요.')
-            break
+            print('{}님 환영합니다.'.format(info['이름']))
+            return
+        elif (info['아이디'] != id) and (info['비밀번호'] == pw):
+            print('아이디가 틀렸습니다.')
+        elif (info['아이디'] == id) and (info['비밀번호'] != pw):
+            print('비밀번호가 틀렸습니다.')
+        
+        input()
+# 중복
+def check_duplicate_code():    
+    while True:       
+        info = input('- 아이디: ')    
+        flag = False  # 중복 플래그          
+        # 중복 검증
+        for p in user:
+            if info == p['아이디']:
+                # 중복됨 
+                print('중복되었습니다 다시 입력하세요')
+                flag = True             
+                break
+        if flag == False:
+            return info
 
-# in 키워드를 통해 key 값의 존재 여부를 체크할 수 있음
+
+
 
 
 # 실행부
 if __name__ == '__main__':
-    
-    while True: 
+    while True:
         show_menu()
+        print(user)
         menu = int(input('메뉴 입력 = > '))
         if menu == 1:
             user_info()
+
         if menu == 2:
             login()
-            break
+            
+        if menu == 3:
+           pass
