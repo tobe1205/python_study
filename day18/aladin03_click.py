466ba826d146e96911c8d1e82efdb444ebd4942e
tobe1205 <tobe1205@hanmail.net>
2021-11-29 11:53:35 +0900

    [21.11.29] # 파이썬 기초 - 크롤링

--- day18/aladin03_click.py
+++ day18/aladin03_click.py
@@ -1,4 +1,3 @@
-
 from selenium import webdriver
 from bs4 import BeautifulSoup
 import time as t
@@ -20,7 +19,7 @@ d = datetime.today()
 file_name = f'알라딘베스트셀러데이터_{d.year}_{d.month}_{d.day}.xlsx'
 
 # 파일 저장 경로
-file_save_path = f'D:/isec_kgw/py_study/{file_name}'
+file_save_path = f'D:/isec_spring1/py_study/{file_name}'
 
 # 엑셀라이브러리 객체 생성
 workbook = xlsxwriter.Workbook(file_save_path)
@@ -38,7 +37,7 @@ worksheet.write('F1', '가격')
 
 
 # 물리드라이버
-browser = webdriver.Chrome('D:/isec_kgw/py_study/c_driver/chromedriver.exe')
+browser = webdriver.Chrome('D:/isec_spring1/py_study/c_driver/chromedriver.exe')
 
 # 브라우저 최대창으로 띄우기
 browser.maximize_window()
@@ -46,61 +45,71 @@ browser.maximize_window()
 # 타겟 사이트로 이동
 browser.get('https://www.aladin.co.kr/shop/common/wbest.aspx?BranchType=1&start=we')
 
-# 현재 페이지 소스코드 불러오기
-html = browser.page_source
-
-soup = BeautifulSoup(html, 'html.parser')
 
 ############ 핵심 로직 ###############
-
-# 베스트 셀러 정보는 div.ss_book_box 에 있음
-# div_book_box_list = soup.select('div.ss_book_box')
-div_book_box_list = soup.find_all('div', class_='ss_book_box')
-
-# print(div_book_box_list[0])
-
 rank = 1
-for boox_box in div_book_box_list:
-    div_book = boox_box.select_one('div.ss_book_list')
-    # print(div_book)
 
-    li_list = div_book.select('li')
+for n in range(2, 8):
+    # 51위~ 100위: #newbg_body > div:nth-child(6) > ul > li:nth-child(3) > a
+    # 101위 ~ 150위: #newbg_body > div:nth-child(6) > ul > li:nth-child(4) > a
 
-    # 책 제목
-    # 사은품이 있으면 1번, 없으면 0번
-    if len(li_list) == 4:
-        title = li_list[0].text
-        info = li_list[1].text
-        price = li_list[2].text
-    else:
-        title = li_list[1].text
-        info = li_list[2].text
-        price = li_list[3].text
+    if n > 2: 
+        # 순위 탭 클릭
+        browser.find_element_by_css_selector(f'#newbg_body > div:nth-child(6) > ul > li:nth-child({n}) > a').click()
 
-    # info 세가지 데이터로 분해
-    info_list =  info.split('|')
-    author, company, pub_date = info_list
+    t.sleep(1.5)
 
-    # price에서 할인 후 가격만 추출
-    price = price.split('(')[0]
-    price = price[price.find('→') + 1 : ].strip()
+    # 현재 페이지 소스코드 불러오기
+    html = browser.page_source
 
-    print(title)
-    print(author.strip())
-    print(company.strip())
-    print(pub_date.strip())
-    print(price)
-    print('=' * 50)
+    soup = BeautifulSoup(html, 'html.parser')
 
-    # 엑셀파일에 저장
-    worksheet.write(f'A{rank+1}', rank)
-    worksheet.write(f'B{rank+1}', title)
-    worksheet.write(f'C{rank+1}', author)
-    worksheet.write(f'D{rank+1}', company)
-    worksheet.write(f'E{rank+1}', pub_date)
-    worksheet.write(f'F{rank+1}', price)
-    
-    rank += 1
+    # 베스트 셀러 정보는 div.ss_book_box 에 있음
+    # div_book_box_list = soup.select('div.ss_book_box')
+    div_book_box_list = soup.find_all('div', class_='ss_book_box')
+
+
+    for boox_box in div_book_box_list:
+        div_book = boox_box.select_one('div.ss_book_list')
+        # print(div_book)
+
+        li_list = div_book.select('li')
+
+        # 책 제목
+        # 사은품이 있으면 1번, 없으면 0번
+        if len(li_list) == 4:
+            title = li_list[0].text
+            info = li_list[1].text
+            price = li_list[2].text
+        else:
+            title = li_list[1].text
+            info = li_list[2].text
+            price = li_list[3].text
+
+        # info 세가지 데이터로 분해
+        info_list =  info.split('|')
+        author, company, pub_date = info_list
+
+        # price에서 할인 후 가격만 추출
+        price = price.split('(')[0]
+        price = price[price.find('→') + 1 : ].strip()
+
+        print(title)
+        print(author.strip())
+        print(company.strip())
+        print(pub_date.strip())
+        print(price)
+        print('=' * 50)
+
+        # 엑셀파일에 저장
+        worksheet.write(f'A{rank+1}', rank)
+        worksheet.write(f'B{rank+1}', title)
+        worksheet.write(f'C{rank+1}', author)
+        worksheet.write(f'D{rank+1}', company)
+        worksheet.write(f'E{rank+1}', pub_date)
+        worksheet.write(f'F{rank+1}', price)
+        
+        rank += 1
 
 # 물리드라이버 종료
 browser.close()
---
+++ day19/gyobo.py
@@ -0,0 +1,110 @@
+from selenium import webdriver
+from bs4 import BeautifulSoup
+import time as t
+
+
+# 날짜처리 라이브러리
+from datetime import datetime
+
+# 엑셀저장 라이브러리 
+import xlsxwriter
+
+# 이미지 다운로드 처리 라이브러리
+import urllib.request as req
+from io import BytesIO
+
+# 오늘 날짜 시간 
+d = datetime.today()
+
+# 파일명
+file_name = f'교보문고베스트셀러_{d.year}_{d.month}_{d.day}.xlsx'
+
+# 파일 저장 경로
+file_save_path = f'D:/isec_kgw/py_study/{file_name}'
+
+# 엑셀라이브러리 객체 생성
+workbook = xlsxwriter.Workbook(file_save_path)
+
+# 엑셀 워크 시트 만들기
+worksheet = workbook.add_worksheet()
+
+# 행 꾸미기 방법
+styles = {
+    'bold' : True,
+    'font_color' : 'red',
+    'bg_color' : 'yellow'
+}
+cell_format = workbook.add_format(styles)
+
+# 엑셀 열이름 (첫줄 정보 생성)
+worksheet.write('A1', '순위', cell_format)
+worksheet.write('B1', '썸네일',cell_format)
+worksheet.write('C1', '책이름',cell_format)
+worksheet.write('D1', '가격',cell_format)
+worksheet.write('E1', '저자',cell_format)
+
+
+# 물리드라이버
+browser = webdriver.Chrome('D:/isec_kgw/py_study/c_driver/chromedriver.exe')
+
+# 브라우저 최대창으로 띄우기
+# browser.maximize_window()
+
+# 브라우저 원하는 사이즈로 키우기
+browser.set_window_size(1280,1080)
+
+# 타겟 사이트로 이동
+browser.get('http://www.kyobobook.co.kr/index.laf')
+
+
+########## 핵심 로직 ############
+
+t.sleep(2)
+
+browser.find_element_by_css_selector('#header > div.navigation_bar > ul.gnb_main.add_1 > li.item_1 > a').click()
+
+t.sleep(2)
+
+browser.find_element_by_css_selector('#bestSeller > div.section.first.on > div > div > a > img').click()
+
+# 현재 페이지 소스코드 불러오기
+html = browser.page_source
+
+soup = BeautifulSoup(html, 'html.parser')
+
+prod_list = soup.select('ul.list_type01 > li')
+rank = 1
+for i in range(20):
+    p = prod_list[i]
+
+    img_tag = p.select_one('.cover > a > img')
+    img_src = img_tag['src']
+    prod_name = p.select_one('.title > a > strong').text.strip()
+    prod_price = p.select_one('.price > strong').text.strip()
+    prod_author = p.select_one('.author').text.strip()
+    author_plus = prod_author.split('저자 더보기')
+    author = author_plus[0]
+    author_split = author.split('|')
+    author_split = author_split[0]
+
+    
+    
+
+
+    print(prod_name)
+    print(prod_price)
+    print(author_split.strip())
+    print('='*40)
+
+    worksheet.write(f'A{rank+1}', rank)
+    img_data = BytesIO(req.urlopen(img_src).read())
+    worksheet.insert_image(f'B{rank+1}', img_src, {'image_data': img_data}) # 셀이름, 이미지위치, 딕셔너리 
+    worksheet.write(f'C{rank+1}', prod_name)
+    worksheet.write(f'D{rank+1}', prod_price)
+    worksheet.write(f'E{rank+1}', author)
+
+    rank += 1
+    
+
+browser.close()
+workbook.close()
